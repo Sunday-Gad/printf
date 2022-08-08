3428d901d04e0e204e57511a1983729395b2da8b
happiness kemuel <happinesskemuel@gmail.com>
2022-08-08 16:09:09 -0700

    adding comment and correcting for betty style

--- main.h
+++ main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 102
+
 /* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
